767434bcce14c8c87cbb859fe431419a29045bf6
Sunconure11 <eyespot7b@aol.com>
2022-04-05 14:32:55 -0400

    Update LightstonesFeatures.java

--- src/main/java/paperdomo101/lightstones/registry/LightstonesFeatures.java
+++ src/main/java/paperdomo101/lightstones/registry/LightstonesFeatures.java
@@ -2,33 +2,39 @@ package paperdomo101.lightstones.registry;
 
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.biome.GenerationSettings;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.OreFeatureConfig;
+import net.minecraft.world.gen.YOffset;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
+import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
+import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
 import paperdomo101.lightstones.Lightstones;
+
+import java.util.Arrays;
 
 @SuppressWarnings("deprecation")
 public class LightstonesFeatures {
 
-    private static final ConfiguredFeature<?, ?> ORE_LIGHTSTONE = Feature.ORE
-            .configure(new OreFeatureConfig(
-                    OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+    private static final ConfiguredFeature<?, ?> OVERWORLD_ORE_LIGHTSTONE = new ConfiguredFeature
+            (Feature.ORE, new OreFeatureConfig(
+                    OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
                     LightstonesBlocks.LIGHTSTONE_ORE.getDefaultState(),
-                    Lightstones.CONFIG.lightstoneOreVeinSize)) // vein size
-            .decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
-                    Lightstones.CONFIG.lightstoneOreBottomOffset, // bottom offset
-                    Lightstones.CONFIG.lightstoneOreMinYLevel, // min y level
-                    Lightstones.CONFIG.lightstoneOreMaxYLevel))) // max y level
-            .spreadHorizontally()
-            .repeat(Lightstones.CONFIG.lightstoneOreVeinsPerChunk); // number of veins per chunk
+                    Lightstones.CONFIG.lightstoneOreVeinSize));
+
+    public static PlacedFeature ORE_LIGHTSTONE = new PlacedFeature(
+            RegistryEntry.of(OVERWORLD_ORE_LIGHTSTONE),
+            Arrays.asList(
+                    CountPlacementModifier.of(Lightstones.CONFIG.lightstoneOreVeinsPerChunk), // number of veins per chunk
+                    SquarePlacementModifier.of(), // spreading horizontally
+                    HeightRangePlacementModifier.uniform(YOffset.aboveBottom(Lightstones.CONFIG.lightstoneOreMinYLevel), YOffset.belowTop(Lightstones.CONFIG.lightstoneOreMaxYLevel))
+            ));
 
     private static final ConfiguredFeature<?, ?> ORE_LIGHTSTONE_EXTRA = Feature.ORE
             .configure(new OreFeatureConfig(
