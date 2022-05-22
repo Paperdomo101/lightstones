195ba97268aeb066b0dcbb115f6f66bac363fc60
Sunconure11 <eyespot7b@aol.com>
2022-05-22 15:20:37 -0400

    Update LightstonesClient.java

--- src/main/java/paperdomo101/lightstones/client/LightstonesClient.java
+++ src/main/java/paperdomo101/lightstones/client/LightstonesClient.java
@@ -2,7 +2,6 @@ package paperdomo101.lightstones.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import paperdomo101.lightstones.api.banner.LightstonesBannerRegistry;
-import paperdomo101.lightstones.registry.LightstonesBannerPatterns;
 
 public class LightstonesClient implements ClientModInitializer {
 
